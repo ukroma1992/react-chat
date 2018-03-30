7379e0a4c34acb1b91cec3832c163191899c8e98
ukroma1992 <ukroma1992@gmail.com>
2018-03-30 14:33:59 +0300

    Added LoginForm and SignupForm components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
 import ChatPage from './ChatPage';
 
-const App = () => (
+const styles = theme => ({
+  root: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    backgroundColor: theme.palette.background.default,
+  },
+});
+
+const App = ({ classes }) => (
   <Router>
-    <Switch>
-      <Route exact path="/(welcome)?" component={WelcomePage} />
-      <Route path="/chat" component={ChatPage} />
-      <Redirect to="/" />
-    </Switch>
+    <div className={classes.root}>
+      <Switch>
+        <Route exact path="/(welcome)?" component={WelcomePage} />
+        <Route path="/chat" component={ChatPage} />
+        <Redirect to="/" />
+      </Switch>
+    </div>
   </Router>
 );
 
-export default App;
+export default withStyles(styles)(App);
